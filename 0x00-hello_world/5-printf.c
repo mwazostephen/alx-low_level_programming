4f050f1cc3e04390e3f8de617ca0f95710b620b4
mwazostephen <mwazostephen0@gmail.com>
2023-09-08 00:43:14 +0300

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-  *main - This is a program that prints a line with printf function
-  *
-  *Return: always (0)
-  *
-  */
+*main-This is a program that prints a line with printf function
+*Return: always (0)
+*/
 int main(void)
 {
+
 printf("with proper grammar, but the outcome is a piece of art,\n"); 
 return (0);
+
 }
